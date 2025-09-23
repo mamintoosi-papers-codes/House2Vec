d1e217ceedb6cee5fb3a288eafba0f2c7bbe5d66
Mahmood Amintoosi <mamintoosi@users.noreply.github.com>
2025-09-23 16:54:25 +0330

    Full run on GPU

--- utils_pyg.py
+++ utils_pyg.py
@@ -54,7 +54,7 @@ def load_dataset(dataset_name: str):
         # filtered_data = data[(data['longitude'] >= 59.4) & (data['longitude'] <= 59.7) &
         #                     (data['latitude'] >= 36.2) & (data['latitude'] <= 36.45)]
         np.random.seed(42)
-        shuffle_indices = np.random.choice(np.arange(filtered_data.shape[0]), size=2000, replace=False,)
+        shuffle_indices = np.random.choice(np.arange(filtered_data.shape[0]), size=10000, replace=False,)
         df = filtered_data.iloc[shuffle_indices].reset_index(drop=True)
 
         df = df.dropna().reset_index(drop=True)
